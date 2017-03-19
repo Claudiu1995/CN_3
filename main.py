3575c2e76462ec87bbbabd17360925fe6910ef36
Geo <george.gripca@gmail.com>
2017-03-19 16:18:38 +0200

    Created vector column b as a matrix n x 1.

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from sum_matrix import sum_matrices
 
 
 def main():
-    matrix_A = read_matrix("a.txt", 10)
-    matrix_B = read_matrix("b.txt", 10)
-    matrix_A_plus_B = read_matrix("aplusb.txt", 20)
+    matrix_A, vect_A = read_matrix("a.txt", 10)
+    matrix_B, vect_B = read_matrix("b.txt", 10)
+    matrix_A_plus_B, vect_A_plus_B = read_matrix("aplusb.txt", 20)
 
     print(matrix_A_plus_B == sum_matrices(matrix_A, matrix_B))
-
 
 main()
